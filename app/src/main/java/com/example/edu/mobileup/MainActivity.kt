189760fafd0eb785b31899b173cae34395e5185a
ramilmavlihanov <Ramil040201@gmail.com>
2024-08-10 16:28:45 +0300

    ADD: navigation, bindings

--- app/src/main/java/com/example/edu/mobileup/MainActivity.kt
+++ app/src/main/java/com/example/edu/mobileup/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.edu.mobileup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.edu.mobileup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navigationController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigationHost = supportFragmentManager
+            .findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
+        navigationController = navigationHost.navController
     }
 }
